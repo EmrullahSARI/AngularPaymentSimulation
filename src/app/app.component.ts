e79ba5cbf81f1e855fc1db4c9f932551563dd538
EMSARI <e.sari@emakina.com.tr>
2020-04-06 11:05:01 +0300

    bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simulatePaymentBasics';
+  name: string;
+
+  onNameInputChange( value: string ) {
+    this.name = value;
+  }
 }
